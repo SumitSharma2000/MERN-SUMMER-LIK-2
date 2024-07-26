44df38b120a0cba30f43abf4ebcebf496707b41a
Sumit <sumitsharma7624@gmail.com>
2024-07-26 14:45:36 +0530

    remove cloud home

--- CloudHome/Backend/routes/filefolderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const {getfilefolders} = require("../controller/filefolderController.js");
-
-const filefolderRouter = express.Router();
-filefolderRouter.route("/").post(getfilefolders);
-
-module.exports = filefolderRouter;
--- CloudHome/Backend/routes/otpRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {generateOtp,verifyOtp} = require('../controller/otpController')
-
-const otpRouter = express.Router();
-
-otpRouter.get('/generate', generateOtp);
-otpRouter.post('/verifyOtp', verifyOtp)
-
-module.exports = otpRouter;
